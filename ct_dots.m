8708ce41baf04ab6fbe1ebcf0227453ff9c8bce5
janarohrer <45822611+janarohrer@users.noreply.github.com>
2019-06-12 17:43:34 +0200

    Add files via upload

--- ct_dots.m
+++ ct_dots.m
@@ -3,7 +3,7 @@ clear all
 
 %% load folder and images
 
-folder = 'CT_STEREO_SCHICHT_0_6MM_0003';
+folder = 'thesis_data/CT_0_6MM_RETAKE';
 [image_ct,info_ct] = dicomfolder(folder);
 %%
 dimx = round(info_ct{1}.PixelSpacing(1)*size(image_ct,1));
@@ -13,13 +13,12 @@ dimz = round(info_ct{1}.SliceThickness*size(image_ct,3));
 %resize voxels to be 1mm x 1mm x 1mm
 slices_ct = imresize3(image_ct,[dimx,dimy,dimz]);
 
-slices_ct = slices_ct(35:294,:,72:336); %to generalize,insert option to set these parameters
-
+slices_ct = slices_ct(35:294,:,72:336); 
 
 %make 3d template of grid cross
-dim = 23; %has to be uneven, >=23 so there are no artefacts in between
+dim = 23; 
 middle = round(dim/2);
-mask_3 = zeros(dim,dim,dim); %make 3d template of grid cross
+mask_3 = zeros(dim,dim,dim);
 mask_3(:,middle-1:middle+1,middle) = 1;
 mask_3(:,middle,middle-1:middle+1) = 1;
 mask_3(middle,:,middle-1:middle+1) = 1;
@@ -29,8 +28,6 @@ mask_3(middle-1:middle+1,middle,:) = 1;
 
 %% crosscorr in 3 dimensions to find crosses (time intensive)
 cross_corr_3 = normxcorr3(mask_3,slices_ct,'same'); 
-
-% normalize
 Mcc = max(max(max(cross_corr_3)));
 cross_corr_3 = cross_corr_3 / Mcc;
 
@@ -64,39 +61,30 @@ cpselect(circ,slices_ct(:,:,central_z));
 iso = [(fixedPoints) central_z]; %iso centre defined by usier input
 delta = (centroids-iso);
 
-mod_xy = mod(delta,20); %not needed for ct so far
+delta_switch(:,1) = delta(:,3);
+delta_switch(:,2) = delta(:,2);
+delta_switch(:,3) = -delta(:,1);
+delta = delta_switch;
+
+
+
 mod_z = mod(delta,20);
 filtered = [];
 for n = 1:size(centroids,1)
-%     if abs(mod_xy(n,1)-20)<5 || mod_xy(n,1)<5 %ckecking all the dim seperate
-%         if abs(mod_xy(n,2)-20)<5 || mod_xy(n,2)<5
             if abs(mod_z(n,3)-20)<3 || mod_z(n,3)<3
                 filtered = cat(1,filtered,delta(n,:));
-            end 
-%         end
-%     end
-        
+            end        
 end
-%3dplot of grid crosses
-% figure;plot3(filtered(:,1),filtered(:,2),filtered(:,3),'o');
-% axis equal
 
-%make sure that all possible points have been found (all pillars to 13):
-%histogram2(filtered(:,1),filtered(:,2),[15,11])
-%edge points will not be detected because of the shape of the mask (might
-%correct later)
 
 % normal grid
-slice_coord = readmatrix('slice-coord');
-grid_norm = [];
 
-for n = -6:6
-    grid_norm = cat(1,grid_norm,[slice_coord n*20*ones(145,1)]);  
-end
+load grid_norm
+grid_norm = grid_norm(146:end,:);
 %
 [Idx] = knnsearch(filtered,grid_norm);
 sorted_ct = filtered(Idx,:);
-sorted_ct = sorted_ct - sorted_ct(923,:); %adjust middle to 0,0,0
+sorted_ct = sorted_ct - sorted_ct(778+145,:); %adjust middle to 0,0,0
 
 % registering using pointCloud objects to use CV toolbox
 pc_ct = pointCloud(sorted_ct);
@@ -105,59 +93,27 @@ pc_norm = pointCloud(grid_norm);
 [tform,ct_reg] = pcregistericp(pc_ct,pc_norm);
 
 [~,D] = knnsearch(ct_reg.Location,grid_norm); %calculate distance of points
-% signx = sign(sorted(:,1)-grid_norm(:,1));
-% signy = sign(sorted(:,2)-grid_norm(:,2));
-% signz = sign(sorted(:,3)-grid_norm(:,3));
-% D_sign = D.*(signz);
 
-% create 3d map
-% D = transpose(D);
-
-% D_slides = [];
-% D_zeros = [];
-% 
-% for n = 1:13
-%     D_slides = D((n-1)*145+1:n*145);
-%     D_temp = [D_slides(1:15*7) 0 D_slides(15*7+1:15*7+13) 0 0 0 ...
-%         D_slides(15*7+14:15*8+9) 0 0 0 0 0 D_slides(15*8+10:15*9+3) ...
-%         zeros(1,7) D_slides(15*9+4:15*9+10) 0 0 0 0];
-%     D_slice = [];
-%     for row=1:11
-%         D_slice=cat(1,D_slice,D_temp((row-1)*15+1:row*15));
-%     end
-%     D_zeros = cat(3,D_zeros,D_slice);
-% end
-% Vq = interp3(D_zeros,'cubic'); %use this to make plot smoother
-% % Vq = D_zeros;
-% figure;
-% slice(Vq,1:size(Vq,2),1:size(Vq,1),1:size(Vq,3));
-% %choose color and add transparency to slices
-% colormap jet;
-% alpha('color');
-% alphamap('rampup');
-% alphamap('decrease',0);
-% colorbar
-
+D_red=D;
+D_red(D>10) = -1; 
+D = D_red;
  
 map = jet;
 
-m = 63/(max(D));
+m = 63/max(D);
 b = 1;
 %
-figure; hold on; title('3D plot')
+figure; hold on; title('Distortion in mm')
 pcshow(pointCloud([0 0 0]));
-colormap jet; colorbar;caxis([min(D) max(D)]);
+colormap jet; colorbar;caxis([0 max(D)]);
 colorbar('Ticks',[0,1,max(D)],...
-         'TickLabels',{0,1,max(D)},'Color','w')
+         'TickLabels',{0,1,1.8385},'Color','w')
 for point = 1:size(D)
+    if D(point)>=0
     color = round(m*D(point)+b);
     plot3(grid_norm(point,1),grid_norm(point,3),-grid_norm(point,2),'+','Color',map(color,:),'MarkerSize',8,'LineWidth',2)
-    
+    end
 end
-
-
-%%
-
 
 % dist. from iso center vs distortion
 
@@ -176,10 +132,10 @@ D_20(D_centre<=200)=D(D_centre<=200);
 
 
 
-%% Scatter Plot
+% Scatter Plot
 figure;
 scatter(D_centre(D~=-1),D(D~=-1),'x')
-title('Scatter Plot of Distortion in CT');
+title('Distortion in CT');
 xlabel('Distance from centre in mm');ylabel('Distortion in mm');
 grid on;
 
